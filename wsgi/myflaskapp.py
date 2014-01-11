a130c459ddea57a8513a969650b11a8e0c1f3b28
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-11 12:49:18 -0500

    Add: tryout with pretty and ugly.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/ugly/')
+def uglyfy():
+	return 'Ugly.'
+
+
+@app.route('/pretty/')
+def uglyfy():
+	return 'Pretty.'
+
+
+if __name__ == '__main__':
     app.run()
 
