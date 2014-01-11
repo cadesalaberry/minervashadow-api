262fadb04b497bf3738e536c6eece87409577f1e
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-11 12:40:34 -0500

    Upd: setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
+try:
+	from setuptools import setup 
+except:
+	from disutils.core import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
-     )
+import minervashadow_api
+
+
+dependencies = ['Flask>=0.7.2', 'MarkupSafe']
+
+setup(
+
+	name='minervashadow_api',
+	version=minervashadow_api.get_version(),
+	description='OpenShift App to make the minervashadow_api available to the public',
+	long_description=open('README.md').read(),
+	url='https://github.com/cadesalaberry/minervashadow-api',
+	author='cadesalaberry',
+	author_email='cadesalaberry@yahoo.com',
+	install_requires=dependencies,
+	classifiers={
+		'Development Status :: 1 - Alpha',
+		'Intended Audience :: Developers',
+		'NaturalLanguage :: English',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.6',
+	}
+)
