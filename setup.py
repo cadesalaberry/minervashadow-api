c73daffc050247a7625a8c83dcaf974b4cddbb4f
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-12 10:17:02 -0500

    Trying reverting to original

--- setup.py
+++ setup.py
@@ -1,22 +1,10 @@
-try:
-	from setuptools import setup 
-except:
-	from disutils.core import setup
+from setuptools import setup
 
-import version
-
-
-dependencies = ['Flask>=0.7.2', 'MarkupSafe']
-
-setup(
-
-	name='minervashadow_api',
-	version=version.get_version(),
-	description='OpenShift App to make the minervashadow-api available to the public',
-	long_description=open('README.md').read(),
-	url='https://github.com/cadesalaberry/minervashadow-api',
-	author='cadesalaberry',
-	author_email='cadesalaberry@yahoo.com',
-	install_requires=dependencies
-)
-
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
