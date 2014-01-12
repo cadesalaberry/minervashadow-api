3519f7bc8482b34dc685e1b86d674dd200131ad4
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-12 10:43:09 -0500

    Add: Trying ugly.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/ugly')
+def uglyfy():
+	return 'Ugly.'
+
 
 if __name__ == "__main__":
     app.run()
