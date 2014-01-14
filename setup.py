525f0b58137dad829b98d9af74dc01444e8a69e8
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-14 15:29:26 -0500

    Add: github tarball link.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dependency_links = [
 'https://pypi.python.org/pypi/minervashadow',
 'https://github.com/cadesalaberry/minervashadow'
 'git+https://github.com/cadesalaberry/minervashadow.git'
-'https://api.github.com/repos/cadesalaberry/minervashadow/tarball'
+'https://api.github.com/repos/cadesalaberry/minervashadow/tarball/master#egg=minervashadow-0.0.3a1'
 ]
 
 setup(
